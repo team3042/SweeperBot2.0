d0b99624855415d9288683d435d449213901a18f
Anthony Brogni <anthonybrog@gmail.com>
2019-06-20 20:23:43 -0500

    Removed Camera

--- src/main/java/org/usfirst/frc/team3042/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team3042/robot/Robot.java
@@ -6,7 +6,6 @@ import org.usfirst.frc.team3042.robot.subsystems.Gyroscope;
 import org.usfirst.frc.team3042.robot.subsystems.LightRing;
 import org.usfirst.frc.team3042.robot.subsystems.LineTracker;
 
-import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -40,21 +39,12 @@ public class Robot extends TimedRobot {
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<Command>();
 
-	CameraServer camera1;
-	CameraServer camera2;
-
 	/** robotInit *************************************************************
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	public void robotInit() {
 		log.add("Robot Init", Log.Level.TRACE);
-
-		camera1 = CameraServer.getInstance();
-		camera2 = CameraServer.getInstance();
-
-		camera1.startAutomaticCapture();
-		camera2.startAutomaticCapture();
 		
 		oi = new OI();
 		/*Autonomous*/
