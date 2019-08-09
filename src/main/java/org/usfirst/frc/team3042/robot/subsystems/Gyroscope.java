381e7d6badcd36ff855313e5c9015bb3224253f7
Anthony Brogni <anthonybrog@gmail.com>
2019-08-09 09:36:47 -0500

    Update Gyroscope.java

--- src/main/java/org/usfirst/frc/team3042/robot/subsystems/Gyroscope.java
+++ src/main/java/org/usfirst/frc/team3042/robot/subsystems/Gyroscope.java
@@ -1,12 +1,12 @@
 package org.usfirst.frc.team3042.robot.subsystems;
 
-import com.analog.adis16448.frc.ADIS16448_IMU;
+//import com.analog.adis16448.frc.ADIS16448_IMU; //Uncomment for ADIS16448 gyroscope (Big Boy)
 
 import org.usfirst.frc.team3042.lib.Log;
 import org.usfirst.frc.team3042.robot.RobotMap;
 import org.usfirst.frc.team3042.robot.commands.Gyroscope_Dashboard;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro; //Uncomment for ADXRS450 gyroscope (Small Boy)
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /** Gyroscope *****************************************************************
@@ -19,8 +19,8 @@ public class Gyroscope extends Subsystem {
 
 	/** Instance Variables ****************************************************/
 	Log log = new Log(LOG_LEVEL, getName());
-	ADIS16448_IMU gyroscope = new ADIS16448_IMU();
-	ADXRS450_Gyro sweeperBotGyro = new ADXRS450_Gyro();
+	//ADIS16448_IMU gyroscope = new ADIS16448_IMU(); //Uncomment for ADIS16448 gyroscope (Big Boy)
+	ADXRS450_Gyro gyroscope = new ADXRS450_Gyro(); //Uncomment for ADXRS450 gyroscope (Small Boy)
 
 	/** Gyroscope *************************************************************/
 	public Gyroscope() {
@@ -38,15 +38,13 @@ public class Gyroscope extends Subsystem {
 	
 	/** Command Methods *******************************************************/
 	public double getAngle() {
-		//return gyroscope.getAngleZ()*GYROSCOPE_SCALE;	
-		return sweeperBotGyro.getAngle() * GYROSCOPE_SCALE;
+		//return gyroscope.getAngleZ()*GYROSCOPE_SCALE;	//Uncomment for ADIS16448 gyroscope (Big Boy)
+		return gyroscope.getAngle() * GYROSCOPE_SCALE; //Uncomment for ADXRS450 gyroscope (Small Boy)
 	}
 	public void reset() {
-		//gyroscope.reset();
-		sweeperBotGyro.reset();
+		gyroscope.reset();
 	}
 	public void calibrate() {
-		//gyroscope.calibrate();
-		sweeperBotGyro.calibrate();
+		gyroscope.calibrate();
 	}
 }
